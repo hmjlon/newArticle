8937e9d6f23a1f1c5eddb5684350bcd4d7c585cc
hmjlon <jiyoung110077@gmail.com>
2025-02-27 17:55:24 +0900

    #1.Redirect Test

---
+++ src/main/java/com/my/articles/dao/ArticleDAO.java
@@ -0,0 +1,49 @@
+package com.my.articles.dao;
+
+import com.my.articles.entity.Article;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+//Article Entity 와 article Table 간 ORM 처리 용도
+//DAO ( Data Access Object)
+@Component
+@Transactional
+public class ArticleDAO {
+    public Object updateArticle;
+    @Autowired
+    EntityManager em;
+
+    public List<Article> getAllArticles(){
+        String sql = "SELECT a FROM Article AS a " +
+                "ORDER BY a.id DESC";
+        List<Article> articles = em.createQuery(sql).getResultList();
+        return articles;
+    }
+
+    public void insertArticle(Article article){
+        em.persist(article);
+
+    }
+
+    public Article getOneArticle(Long id){
+        return em.find(Article.class,id);
+    }
+
+    public void deleteArticle(Long id) {
+        //Id 로 찾기
+        Article article = em.find(Article.class,id);
+        em.remove(article);
+    }
+
+    public void updateArticle(Article article){
+        Article original = em.find(Article.class, article.getId());
+        //변경하면 끝
+        original.setTitle(article.getTitle());
+        original.setContent(article.getContent());
+
+    }
+}
