ff231aeecde45ef09c454127a408147e7987e4f2
hmjlon <jiyoung110077@gmail.com>
2025-03-04 11:51:45 +0900

    #7.Comment CRUD 완성

---
+++ src/main/java/com/my/articles/controller/ApiTestController.java
@@ -0,0 +1,73 @@
+package com.my.articles.controller;
+
+import com.my.articles.dto.LoginDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class ApiTestController {
+    @GetMapping("/api")
+    public String apiForm() {
+        return "/test/api";
+    }
+
+    // @RequestBody : Json 형식을 -> Object(DTO)
+    // Return Type : ResponseEntity -> Object 전송
+    @PostMapping("/apiPostTest")
+    public ResponseEntity<String> apiPostTest(
+            @RequestBody LoginDTO dto) {
+        String info = dto.getUserid() + " / " + dto.getPassword();
+        System.out.println("====== info : " + info);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(info);
+    }
+
+    @PostMapping("/getResponse")
+    @ResponseBody
+    public LoginDTO postResponse() {
+        return new LoginDTO("안유진", "1233");
+    }
+
+    @PostMapping("/apiPostArrayTest")
+    public ResponseEntity<Map<String, String>> apiPostArraryTest(
+            @RequestBody List<LoginDTO> dtos
+            ) {
+        dtos.stream().forEach(x -> System.out.println(x));
+        Map<String, String> userData = new HashMap<>();
+        userData.put("id", dtos.get(1).getUserid());
+        userData.put("pw", dtos.get(1).getPassword());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userData);
+    }
+
+    @DeleteMapping("/apiTest")
+    @ResponseBody
+    public String apiHttpMethodTest() {
+        // 삭제 처리
+        String message = "Delete Mapping";
+        return message;
+    }
+
+    @PatchMapping("/apiTest")
+    @ResponseBody
+    public String apiPatchTest() {
+        // 삭제 처리
+        String message = "Patch Mapping";
+        return message;
+    }
+    @PutMapping("/apiTest")
+    @ResponseBody
+    public String apiPutTest() {
+        // 삭제 처리
+        String message = "Put Mapping";
+        return message;
+    }
+}
---
+++ src/main/java/com/my/articles/service/CommentService.java
@@ -0,0 +1,47 @@
+package com.my.articles.service;
+
+import com.my.articles.dao.CommentDAO;
+import com.my.articles.dto.CommentDTO;
+import com.my.articles.entity.Comment;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CommentService {
+    private final CommentDAO commentDAO;
+
+    public CommentService(CommentDAO commentDAO) {
+        this.commentDAO = commentDAO;
+    }
+
+    public Long deleteComment(Long id) {
+        return commentDAO.deleteComment(id);
+    }
+
+    public void insertComment(Long articleId, CommentDTO dto) {
+        commentDAO.insertComment(articleId,
+                CommentDTO.fromDto(dto));
+    }
+
+    public Map<String, Object> findByCommentId(Long commentId) {
+        Comment comment = commentDAO.findByCommentId(commentId);
+        Map<String,Object>data = new HashMap<>();
+        if(ObjectUtils.isEmpty(comment)){
+            data.put("articleId",null);
+            data.put("dto",null);
+
+        }else{
+            data.put("articleId",comment.getArticle().getId());
+            CommentDTO commentDTO = null;
+            data.put("dto",commentDTO.fromEntity(comment));
+    }
+        return data;
+}
+
+    public void updateComment(CommentDTO dto) {
+        commentDAO.updateComment(CommentDTO.fromDto(dto));
+    }
+}
